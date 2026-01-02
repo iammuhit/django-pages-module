accb608a8eaf2c12a16030a19d46d89120089e9b
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-02 09:37:58 +0100

    register type and page admin

--- admin.py
+++ admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.modules.pages.models import Page, Type
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at', 'updated_at']
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type__name', 'is_enabled', 'publish_at', 'created_at', 'updated_at']
+    readonly_fields = ['route_name']
+    prepopulated_fields = {'slug': ['title']}
+
+    fieldsets = [
+        ('General', {
+            'fields': ['type', 'title', 'slug', 'content'],
+        }),
+
+        ('SEO', {
+            'classes': ['collapse'],
+            'fields': ['meta_title', 'meta_description', 'meta_keywords'],
+        }),
+        
+        ('Options', {
+            'classes': ['collapse'],
+            'fields': ['is_enabled', 'publish_at', 'is_home', 'is_visible', 'is_exact', 'parent', 'route_name'],
+        }),
+    ]
