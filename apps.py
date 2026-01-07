85848f93747669b03a576014af4209f958b6cba9
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-08 00:10:04 +0100

    update verbose-name to app config

--- apps.py
+++ apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.modules.pages'
-    verbose_name = _('pages')
+    verbose_name = _('Pages')
