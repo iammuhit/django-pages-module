623a6d2d564e9f337a0d45881b906dce88f9510e
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-15 02:08:20 +0100

    improve page handling

--- src/mayacms/contrib/pages/views.py
+++ src/mayacms/contrib/pages/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
 
@@ -14,9 +15,15 @@ class PageView(generic.DetailView):
         path = self.kwargs.get('path', '').strip('/')
         path = f'/{path}'
 
-        if path == '/':
-            page = Page.objects.get(Q(path=path) | Q(is_home=True))
-        else:
-            page = Page.objects.get(path=path, is_home=False)
+        if path == 'favicon.ico':
+            raise Http404()
+
+        try:
+            if path == '/':
+                page = Page.objects.get(Q(path=path) | Q(is_home=True))
+            else:
+                page = Page.objects.get(path=path, is_home=False)
+        except Exception:
+            raise Http404()
 
         return page
